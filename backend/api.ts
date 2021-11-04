d69146f286a8b30142995d504a68138922d94731
Lukas <lukas@leucali.net>
2021-11-04 20:54:14 +0100

    Added Status and Welcome Endpoint

--- backend/api.ts
+++ backend/api.ts
@@ -2,17 +2,23 @@ import express from 'express'
 import { echoController } from './controllers/echo';
 import * as logger from './util/logger';
 import cors from 'cors';
+import { statusController } from './controllers/status';
+import { welcomeController } from './controllers/main';
 const router = express.Router();
 
 router.use(cors());
 router.use(express.json());
 router.use(logger.logToConsole);
 
+router.use('/', welcomeController);
+router.use('/status', statusController);
 router.use('/echo', echoController);
 
+//404 Route
 router.use((_req: express.Request, res: express.Response) => {
   res.status(404);
-  res.send('Route does not exist');
+  res.json({"status": "Route does not exist"});
+  res.send();
 });
 
 export { router as apiRouter }
