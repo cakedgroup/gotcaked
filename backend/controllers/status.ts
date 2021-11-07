9fc945bbb35931db2397b6c1379055d9d648e189
Lukas <lukas@leucali.net>
2021-11-07 21:34:21 +0100

    Moved to the correct servie

--- backend/controllers/status.ts
+++ backend/controllers/status.ts
@@ -1,28 +1,17 @@
 import express from "express";
-import { getCounter } from "../services/status";
-import { getVersion } from "../util/version";
+import { getCurrentStatus } from "../services/status";
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    let tables = ["user", "recipe", "tag"];
 
-    Promise.all([getCounter(tables[0]), getCounter(tables[1]), getCounter(tables[2])]).then(counter => {
-        //Build JSON Object
-        let status = {
-            "version": getVersion(),
-            "userCount": counter[0],
-            "recipeCount": counter[1],
-            "commentsCount": "-",
-            "tagsCount": counter[2],
-            "categoriesCount": "-"
-        }
-
-        //Response
+    getCurrentStatus().then(status => {
         res.status(200);
         res.json(status);
     }).catch(err => {
+        res.status(500);
+        res.send();
         console.log(err);
-    });
+    })
 });
 
 
--- backend/services/status.ts
+++ backend/services/status.ts
@@ -1,9 +1,19 @@
 import { getCounterTable } from "../models/status";
+import { getVersion } from "../util/version";
 
-export function getCounter(table: string): Promise<Number> {
+export function getCurrentStatus(): Promise<any> {
+    let tables = ["user", "recipe", "tag"];
     return new Promise((resolve, reject) => {
-        getCounterTable(table).then(counter => {
-            resolve(counter);
+        Promise.all([getCounterTable(tables[0]), getCounterTable(tables[1]), getCounterTable(tables[2])]).then(counter => {
+            let status = {
+                "version": getVersion(),
+                "userCount": counter[0],
+                "recipeCount": counter[1],
+                "commentsCount": "-",
+                "tagsCount": counter[2],
+                "categoriesCount": "-"
+            }
+            resolve(status);
         }).catch(err => {
             reject(err);
         });
