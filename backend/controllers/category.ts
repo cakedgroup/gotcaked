0ee73c8b9184e13e74d63a76042e495625001bbf
Tim Horlacher <tim.horlacher@protonmail.com>
2021-11-16 11:49:36 +0100

    Added category endpoints

--- backend/controllers/category.ts
+++ backend/controllers/category.ts
@@ -1,32 +1,67 @@
 import express from 'express';
+import * as categoryService from '../services/category';
+import { errorHandler } from '../util/errorHandler';
 
 const router = express.Router();
 
+// @route   GET api/categories/
+// @desc    Get all categories
+// @access  Public
 router.get('/', (req, res) => {
-    res.status(501);
-    res.send('To be implemented.');
+    categoryService.getAllCategories().then(categories => {
+        res.status(200).json(categories);
+    }).catch(err => {
+        errorHandler(err, req, res);
+    });
 });
 
+// @route   POST api/categories/
+// @desc    Create a category
+// @access  Admin
 router.post('/', (req, res) => {
-    res.status(501);
-    res.send('To be implemented.');
+    categoryService.createCategory(req.body).then(category => {
+        res.status(201).json(category);
+    }).catch(err => {
+        errorHandler(err, req, res);
+    });
 });
 
-router.get('/:id', (req, res) => {
-    res.status(501);
-    res.send('To be implemented.');
+// @route   GET api/categories/:name
+// @desc    Get a category by name
+// @access  Public
+router.get('/:name', (req, res) => {
+    categoryService.getCategoryById(req.params.name).then(category => {
+        res.status(200).json(category);
+    }).catch(err => {
+        errorHandler(err, req, res);
+    });
 });
 
-router.patch('/:id', (req, res) => {
-    res.status(501);
-    res.send('To be implemented.');
+// @route   PUT api/categories/:name
+// @desc    Update a category
+// @access  Admin
+router.put('/:name', (req, res) => {
+    categoryService.updateCategory(req.params.name, req.body).then(category => {
+        res.status(200).json(category);
+    }).catch(err => {
+        errorHandler(err, req, res);
+    });
 });
 
-router.delete('/:id', (req, res) => {
-    res.status(501);
-    res.send('To be implemented.');
+// @route   DELETE api/categories/:name
+// @desc    Delete a category
+// @access  Admin
+router.delete('/:name', (req, res) => {
+    categoryService.deleteCategory(req.params.name).then(() => {
+        res.status(204).send();
+    }).catch(err => {
+        errorHandler(err, req, res);
+    });
 });
 
+// @route   GET api/categories/random
+// @desc    Get a random category
+// @access  Public
 router.get('/random', (req, res) => {
     res.status(501);
     res.send('To be implemented.');
