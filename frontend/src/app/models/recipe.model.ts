60525bf576f4cef6b6773bb7d116cfed56231d1c
Lukas <lukas@leucali.net>
2021-11-30 07:12:16 +0100

    Updated Models

--- frontend/src/app/models/recipe.model.ts
+++ frontend/src/app/models/recipe.model.ts
@@ -1,10 +1,12 @@
 import { Ingredients } from "./ingredients.model";
+import { Tag } from "./tag.model";
 
 export interface Recipe {
     id: string;
     name: string;
     description: string;
     ingredients: Ingredients[];
+    tags: Tag[];
     preparation: string;
     createdAt: Date;
     time: Number;
