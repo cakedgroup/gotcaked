3926c26a231c5cd43751558a464bb56f5034f495
Lukas <lukas@leucali.net>
2021-12-02 21:37:35 +0100

    Basic Homepage & Errorpage

---
+++ frontend/src/app/shared/components/user-preview/user-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-preview',
+  templateUrl: './user-preview.component.html',
+  styleUrls: ['./user-preview.component.css']
+})
+export class UserPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
