c74c32c613ca1d3e1967a79a4ce62fef6c2c47f3
Lukas <lukas@leucali.net>
2021-12-10 17:06:44 +0100

    Added Guard

--- frontend/src/app/core/services/authGuard.ts
+++ frontend/src/app/core/services/authGuard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { ActivatedRoute, CanActivate, Router } from '@angular/router';
 import * as JWTUtils from '../utils/jwt-utils';
 import { AuthService } from './auth.service';
 
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 export class isLoggedIn implements CanActivate {
   private isLoggedIn: boolean = false;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) {
     this.getAuthStatus();
   }
 
@@ -23,6 +23,7 @@ export class isLoggedIn implements CanActivate {
       if (token !== null && token !== undefined && token !== "" && token !== "null") {
         this.isLoggedIn = true;
       } else {
+        this.router.navigate(['/auth/login']);
         this.isLoggedIn = false;
       }
     });
--- frontend/src/app/pages/user/user-routing.module.ts
+++ frontend/src/app/pages/user/user-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { isLoggedIn } from 'src/app/core/services/authGuard';
 import { UserLikedPageComponent } from './user-liked-page/user-liked-page.component';
 import { UserProfilePageComponent } from './user-profile-page/user-profile-page.component';
 
 const routes: Routes = [
   {
     path: 'liked',
+    canActivate: [isLoggedIn],
     component: UserLikedPageComponent
   },
   {
