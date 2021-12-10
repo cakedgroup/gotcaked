e072ae79bbca3ba7f903feabbfbd5b9c48ffa614
Tim Horlacher <trimin.nurum@gmail.com>
2021-12-10 13:04:38 +0100

    Added error handling

--- frontend/src/app/pages/tag/tag-page/tag-page.component.ts
+++ frontend/src/app/pages/tag/tag-page/tag-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/core/services/api.service';
+import { Tag } from 'src/app/models/tag.model';
 import { Recipe } from '../../../models/recipe.model';
 
 @Component({
@@ -12,7 +13,7 @@ export class TagPageComponent implements OnInit {
   tag: string;
   recipes: Recipe[];
 
-  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+  constructor(private apiService: ApiService, private router : Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.getParams();
@@ -30,6 +31,9 @@ export class TagPageComponent implements OnInit {
     this.apiService.getRecipesByTag(this.tag).subscribe(
       (recipes: Recipe[]) => {
         this.recipes = recipes;
+      },
+      (error) => {
+        this.router.navigate(['/404'], { skipLocationChange: true });
       }
     );
   }
