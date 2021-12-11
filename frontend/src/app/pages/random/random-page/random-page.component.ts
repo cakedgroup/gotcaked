149b32d6eda041f4355c2aa6e02040fe7af87fba
Lukas <lukas@leucali.net>
2021-12-11 15:21:26 +0100

    basic random page

---
+++ frontend/src/app/pages/random/random-page/random-page.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../../../models/recipe.model';
+import { ApiService } from '../../../core/services/api.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-random-page',
+  templateUrl: './random-page.component.html',
+  styleUrls: ['./random-page.component.css']
+})
+export class RandomPageComponent implements OnInit {
+  type: string = '';
+  name: string = '';
+  recipe: Recipe;
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.getParams();
+  }
+
+  getParams() {
+    //Updating Route-Stuff for new Route/Param
+    this.route.params.subscribe(params => {
+      this.type = params.type;
+      this.name = params.name;
+      this.loadRandomRecipe();
+    });
+  }
+
+  loadRandomRecipe() {
+    if (this.type === 'category') {
+      this.getRandomRecipeByCategory(this.name);
+    }
+    else if (this.type === 'tag') {
+      this.getRandomRecipeByTag(this.name);
+    }
+    else {
+      this.getRandomRecipe();
+    }
+  }
+
+  getRandomRecipeByCategory(category: string) {
+    this.apiService.getRandomRecipeByCategory(category).subscribe(recipe => {
+      this.recipe = recipe;
+    });
+  }
+
+  getRandomRecipeByTag(tag: string) {
+    this.apiService.getRandomRecipeByTag(tag).subscribe(recipe => {
+      this.recipe = recipe;
+    });
+  }
+
+  getRandomRecipe() {
+    this.apiService.getRandomRecipe().subscribe(recipe => {
+      this.recipe = recipe;
+    });
+  }
+}
