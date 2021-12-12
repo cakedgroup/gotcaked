f61db4a5ab9217a278590655ea75c583416c1021
Lukas <lukas@leucali.net>
2021-12-12 11:30:10 +0100

    Added Tags overview

--- frontend/src/app/pages/tag/tag.module.ts
+++ frontend/src/app/pages/tag/tag.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { TagPageComponent } from './tag-page/tag-page.component';
 import { TagRoutingModule } from './tag-routing.module';
 import { SharedModule } from '../../shared/shared.module';
+import { TagOverviewComponent } from './tag-overview/tag-overview.component';
 
 
 
 @NgModule({
   declarations: [
-    TagPageComponent
+    TagPageComponent,
+    TagOverviewComponent
   ],
   imports: [
     CommonModule,
